f5944e653ebe07de396dc6c438c268aed0c1fddf
harukiobata <barukan0913@gmail.com>
2025-02-26 17:38:53 +0900

    add/edit.scss,edit.html.erbのコード大幅変更,new、show.scssの微調整,new.html.erbのコードの構文修正,バリデーションの表示修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
 
     def end_date_is_not_before_start_date
         if end_date.present? && start_date.present? && end_date < start_date
-            errors.add(:end_date, "は開始日より後の日付でなければなりません")
+            errors.add(:end_date, "は開始日より後の日付にしてください")
         end
     end
 end
